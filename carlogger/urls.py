9b4ec97ca95bf9bb274d7c1d6ee5e78e0564dee7
Pablo Klijnjan <pabloklijnjan@gmail.com>
2013-05-10 20:07:34 +0300

    Adding index

--- carlogger/urls.py
+++ carlogger/urls.py
@@ -1,17 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from carlogger.views import HomePageView
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'carlogger.views.home', name='home'),
-    # url(r'^carlogger/', include('carlogger.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'^admin/', include(admin.site.urls)),
 )
