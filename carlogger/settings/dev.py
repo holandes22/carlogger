4bf72ac066beae693ae2888f9f3799fb2b41fa5e
Pablo Klijnjan <pabloklijnjan@gmail.com>
2013-05-14 18:09:29 +0300

    Added URLs to be served by django from local instead of aws
        - Ease up development

--- carlogger/settings/dev.py
+++ carlogger/settings/dev.py
@@ -4,6 +4,8 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 
+STATIC_URL = '/static/'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
--- carlogger/urls.py
+++ carlogger/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+
 from carlogger.views import HomePageView
 
 admin.autodiscover()
@@ -9,3 +11,9 @@ urlpatterns = patterns('',
     url(r'^cars/', include('carlogger.cars.urls', namespace='cars')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+                        (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+                            {'document_root': settings.STATIC_ROOT})
+                            )
